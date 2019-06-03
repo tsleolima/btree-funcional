0798f20bd6b3dedb787d4fd864d0025598f81801
Leonardo Lima <leonardo.lima.silva@ccc.ufcg.edu.br>
2019-06-03 17:03:10 -0300

    search

--- b-tree.hs
+++ b-tree.hs
@@ -1,6 +1,19 @@
 -- Ordem dada pelo valor do INT
 
-data Tree a = Nil Int | Leaf Int [a] | Node Int [a] [Tree a] deriving Show
+data BTree a = Nil Int | Leaf Int [a] | Node Int [a] [BTree a] deriving Show
 
-a = (Node 3 [30] [Leaf 3 [20],Leaf 3 [35,38]])
+a = (Node 2 [30] [Leaf 2 [20],Leaf 2 [35,38,40]])
+ 
+search (Nil _) _ = False
+search (Leaf _ []) _ = False
+search (Leaf o (x:xs)) elem 
+ | x == elem = True
+ | x > elem  = False
+ | otherwise = search (Leaf o xs) elem
 
+search (Node _ [] []) elem = False
+search (Node _ [] (x:xs)) elem = search x elem
+search (Node o (x:xs) (y:ys)) elem 
+ | x == elem = True
+ | x > elem  = search y elem
+ | otherwise = search (Node o xs ys) elem
