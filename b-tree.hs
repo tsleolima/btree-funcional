4e15f62ca2133eead1fa8c17f40ed17a70b3ae2a
Leonardo Lima <leonardo.lima.silva@ccc.ufcg.edu.br>
2019-06-05 00:16:52 -0300

    +

--- b-tree.hs
+++ b-tree.hs
@@ -2,7 +2,7 @@
 
 data BTree a = Nil Int | Leaf Int [a] | Node Int [a] [BTree a] deriving Show
 
-a = (Node 2 [30] [Leaf 2 [20],Leaf 2 [35,38,40]])
+a = (Node 3 [30] [Leaf 3 [20],Leaf 3 [35,38,40]])
  
 search (Nil _) _ = False
 search (Leaf _ []) _ = False
@@ -17,3 +17,22 @@ search (Node o (x:xs) (y:ys)) elem
  | x == elem = True
  | x > elem  = search y elem
  | otherwise = search (Node o xs ys) elem
+
+insert t x 
+ | isFull t = insert (split t) x
+ | otherwise = insert t x
+
+isFull (Nil m) = False
+isFull (Leaf _ []) = False
+isFull (Leaf o xs)
+ | ((o -1) <= length xs) = True
+ | otherwise = False
+
+isFull (Node o xs _)
+ | (o -1) <= length xs = True
+ | otherwise = False
+
+split (Node o xs ys) = (Node o xs [Leaf o leftHalf, Leaf o rigthHalf])
+
+leftHalf xs = take (ceiling ((length xs)/2))
+rigthHalf xs = drop (ceiling ((length xs/2))
