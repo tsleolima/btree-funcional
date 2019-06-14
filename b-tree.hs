64040a5fc56306993281185514f264abe99f38fd
Leonardo Lima <leonardo.lima.silva@ccc.ufcg.edu.br>
2019-06-14 13:36:12 -0300

    insert

--- b-tree.hs
+++ b-tree.hs
@@ -3,6 +3,7 @@
 data BTree a = Nil Int | Leaf Int [a] | Node Int [a] [BTree a] deriving Show
 
 a = (Leaf 3 [])
+b = (Node 3 [2,5] [Leaf 3 [1],Leaf 3 [3,4],Leaf 3 [10]])
  
 insertOrdered a [] = [a]
 insertOrdered a bs'@(b:bs)
@@ -20,19 +21,24 @@ insert' tree@(Leaf o chaves@(k:ks)) elem
  | elem < k  = Leaf o (elem:chaves)
  | elem > k  = Leaf o (insertOrdered elem chaves)
 
+insert' (Node o [] (t:ts)) elem = if isFull tree then insert' (split tree) elem
+                                  else Node o [] [(insert' t elem)]
+                                  where tree = insert' t elem
+
 insert' node@(Node o chaves@(x:xs) (y:ys)) elem 
  | elem == x = node
- | elem < x = if isFull y then (Node o (mediana:x:xs) [left,right,ys]) elem
-              else (Node o chaves (insert' y elem):ys)
+ | elem < x = if isFull tree then insert' (Node o (mediana:x:xs) (left:right:ys)) elem
+              else (Node o chaves ((insert' y elem):ys))
  | elem > x = Node o (x:novoXs) (y:novoYs)
- where Node _ [mediana] [left,right] = split y
+ where Node _ [mediana] [left,right] = split tree
        Node _ novoXs novoYs = insert' (Node o xs ys) elem
+       tree = insert' y elem
 
 split (Leaf o chaves) = (Node o [mediana] [Leaf o left, Leaf o right])
  where left = leftHalf chaves
        mediana:right = halfRight chaves
 
-split (Node o chaves trees) = Node o [mediana] [Node o c1 t1, Node o c2, t2]
+split (Node o chaves trees) = Node o [mediana] [Node o left t1, Node o right t2]
  where left = leftHalf chaves
        mediana:right = halfRight chaves
        t1 = leftHalf trees
